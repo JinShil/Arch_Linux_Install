199edac73809381cb5c48d1205832cc2bf1d65b0
Mike <slavo5150@yahoo.com>
2015-07-18 23:24:26 +0900

    Finished basic install

--- install.sh
+++ install.sh
@@ -11,3 +11,33 @@ mount /dev/sda1 /mnt
 pacstrap /mnt base base-devel
 
 genfstab -U -p /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+export LANG=en_US.UTF-8
+
+ls -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+hwclock --systohc --utc
+
+echo ArchLinux > /etc/hostname
+
+sed -i 's/127.0.0.1\tlocalhost.localdomain\tlocalhost/127.0.0.1\tlocalhost.localdomain\tlocalhost\tArchLinux/g' /etc/hosts
+sed -i 's/::1\t\tlocalhost.localdomain\tlocalhost/::1\t\tlocalhost.localdomain\tlocalhost\tArchLinux/g' /etc/hosts
+
+# systemctl enable dhcpcd@enp0s3.service
+
+# set root password
+
+pacman -S --noconfirm grub-bios
+
+grub-install /dev/sda
+
+mkinitcpio -p linux
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
+
+umount /mnt
