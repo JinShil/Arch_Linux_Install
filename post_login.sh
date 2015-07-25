e939c1eb561795cf01af96a8bdad6c3d0df0e6ea
Mike <slavo5150@yahoo.com>
2015-07-25 12:18:07 +0900

    Modify xinitrc

--- post_login.sh
+++ post_login.sh
@@ -4,5 +4,11 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 git config --global user.email ""
 git config --global user.name "JinShil"
 
-sed -i 's/twm &\nxclock -geometry 50x50-1+1 &\nxterm -geometry 80x50+494+51 &\nxterm -geometry 80x20+494-0 &\nexec xterm -geometry 80x66+0+0 -name login\n/exec startkde\n/g' ~/.xinitrc
+sed -i 's/^twm/#twm/g' ~/.xinitrc
+sed -i 's/^xclock -geometry/#xclock -geometry/g'  ~/.xinitrc
+sed -i 's/^xterm -geometry/#xterm -geometry/g'  ~/.xinitrc
+sed -i 's/^exec xterm/#exec xterm/g' ~/.xinitrc
+
+echo "exec startkde" >>  ~/.xinitrc
+
 
