bd983c4dec4552a1c64cc28a85b96c8e5d424d0a
Mike <slavo5150@yahoo.com>
2015-07-25 10:50:58 +0900

    remove run script

--- post_login.sh
+++ post_login.sh
@@ -35,17 +35,5 @@ if [ -d /etc/X11/xinit/xinitrc.d ] ; then
 fi
 
 exec startkde
+
 EOF
-
-# Add run command
-cat > /usr/bin/run <<EOF
-#!/bin/bash
-
-if [ $# -gt 0 ] ; then
-    ($@ &) &>/dev/null
-else
-    echo "missing argument"
-fi
-EOF
-
-chmod +x /usr/bin/run
