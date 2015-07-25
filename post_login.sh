1b053ec7bd476ab487d91f9cb3ba89cdff561fc6
Mike <slavo5150@yahoo.com>
2015-07-25 11:50:54 +0900

    configure git

--- post_login.sh
+++ post_login.sh
@@ -1,3 +1,8 @@
 # Create xinit script
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+git config --global user.email ""
+git config --global user.name "JinShil"
+
 sed -i 's/twm &\nxclock -geometry 50x50-1+1 &\nxterm -geometry 80x50+494+51 &\nxterm -geometry 80x20+494-0 &\nexec xterm -geometry 80x66+0+0 -name login\n/exec startkde\n/g' ~/.xinitrc
+
