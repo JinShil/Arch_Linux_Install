c0ecb6e4ac5e5ab2708813a7316b0b48e159c26b
Mike <slavo5150@yahoo.com>
2015-07-25 10:05:15 +0900

    Added post_login script

---
+++ post_login.sh
@@ -0,0 +1,51 @@
+# Create xinit script
+cat > /home/mike/.xinitrc <<EOF
+#!/bin/sh
+
+userresources=$HOME/.Xresources
+usermodmap=$HOME/.Xmodmap
+sysresources=/etc/X11/xinit/.Xresources
+sysmodmap=/etc/X11/xinit/.Xmodmap
+
+# merge in defaults and keymaps
+
+if [ -f $sysresources ]; then
+    xrdb -merge $sysresources
+fi
+
+if [ -f $sysmodmap ]; then
+    xmodmap $sysmodmap
+fi
+
+if [ -f "$userresources" ]; then
+    xrdb -merge "$userresources"
+fi
+
+if [ -f "$usermodmap" ]; then
+    xmodmap "$usermodmap"
+fi
+
+# start some nice programs
+
+if [ -d /etc/X11/xinit/xinitrc.d ] ; then
+ for f in /etc/X11/xinit/xinitrc.d/?*.sh ; do
+  [ -x "$f" ] && . "$f"
+ done
+ unset f
+fi
+
+exec startkde
+EOF
+
+# Add run command
+cat > /usr/bin/run <<EOF
+#!/bin/bash
+
+if [ $# -gt 0 ] ; then
+    ($@ &) &>/dev/null
+else
+    echo "missing argument"
+fi
+EOF
+
+chmod +x /usr/bin/run
