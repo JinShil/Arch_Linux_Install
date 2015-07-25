38f9acb59400ea3ecd4afd75f90443448d2833c6
Mike <slavo5150@yahoo.com>
2015-07-25 11:39:08 +0900

    Add startkde to xinitrc

--- post_login.sh
+++ post_login.sh
@@ -1,3 +1,3 @@
 # Create xinit script
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-
+sed -i 's/twm &\nxclock -geometry 50x50-1+1 &\nxterm -geometry 80x50+494+51 &\nxterm -geometry 80x20+494-0 &\nexec xterm -geometry 80x66+0+0 -name login\n/exec startkde\n/g' ~/.xinitrc
